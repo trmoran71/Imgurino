385a20560bea9af39cf65eaf6f302e0bcbc584c4
Terry Moran <trmoran@dcentral.org>
2014-02-09 13:02:15 -0700

    Initial Commit

---
+++ Imgurino/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Imgurino
+//
+//  Created by Terence Moran on 2/9/14.
+//  Copyright (c) 2014 Dcentral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Imgurino/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Imgurino
+//
+//  Created by Terence Moran on 2/9/14.
+//  Copyright (c) 2014 Dcentral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
